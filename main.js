29aef53808dc06fe35919fac9f80d61b4714db97
Zach <zjanssen23@gmail.com>
2023-10-22 09:19:11 -0500

    creating normalize function

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('hello world');
+
+function normalizeURL(url) {
+    // function takes in a string and needs to be the same each time it is called with the same input
+    // 1. remove trailing slashes
+    // 2. remove http:// or https://
+    // 3. remove www.
+    // 4. remove everything after the first slash
+    // 5. remove trailing slashes
+}
+
+url = normalizeURL('http://blog.boot.dev/path')
+
