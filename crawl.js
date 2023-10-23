a356b6c7b01de714fe44c73597bb302dcb3590cf
Zach <zjanssen23@gmail.com>
2023-10-23 18:52:40 -0500

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,24 +1,48 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
     console.log(`actively crawling: ${currentURL}`)
+
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+
+    }
+
+    const normalizeCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizeCurrentURL] > 0){
+        pages[normalizeCurrentURL]++
+        return pages
+    }
+
+    pages[normalizeCurrentURL] = 1
+
 
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
-            return
+            return pages
         }
         
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return pages
         }
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL} `)
     }
-    
+    return pages
 }
 
 
